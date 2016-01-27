22fdb2da0bff54c525cf9113426c014513f82a1e
felix <felix@stebra.se>
2016-01-27 16:54:19 +0100

    backend mail function test

---
+++ Website2016/App_Code/mail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Text;
+
+/// <summary>
+/// Summary description for mail
+/// </summary>
+public class mail
+{
+    public mail()
+    {
+
+
+        SmtpClient client = new SmtpClient();
+        client.Port = 25;
+        client.Host = "smtp.gmail.com";
+        client.EnableSsl = true;
+        client.Timeout = 10000;
+        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+        client.UseDefaultCredentials = false;
+        client.Credentials = new System.Net.NetworkCredential("felix.freye@gmail.com", "moffeludd4");
+
+        MailMessage mm = new MailMessage("donotreply@domain.com", "felix.freye@gmail.com", "test", "test");
+        mm.BodyEncoding = UTF8Encoding.UTF8;
+        mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+        client.Send(mm);
+    }
+}
